d4abee6264453b66237e5feade5ecda12eed9938
KIM DOYOON <115680101+doyoon323@users.noreply.github.com>
2026-06-24 02:55:50 +0900

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int i = digits.length-1; 
+        int n = i+1; 
+        digits[i]++; 
+        if(digits[i]!=10) return digits; 
+
+        while(i>0){
+            if(digits[i]==10){
+                digits[i] = 0;
+                digits[i-1]++; 
+            }else return digits; 
+            i--; 
+        }
+        if(digits[0]==10){
+            int[] res = new int[n+1];
+            digits[0]=0; 
+            res[0]=1; 
+            for(int j=0;j<n;j++){
+                res[j+1] = digits[j]; 
+            }
+            return res; 
+        }else return digits; 
+    }
+}
