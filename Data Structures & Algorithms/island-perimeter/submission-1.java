b3c42dafa978fcbebdad27d038e3ab6853d730a6
KIM DOYOON <115680101+doyoon323@users.noreply.github.com>
2026-06-26 16:07:13 +0900

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        int r = grid.length;
+        int c = grid[0].length;
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                    perimeter += 4;
+
+                    if (i < r-1 && grid[i+1][j] == 1)  perimeter -= 2;//top
+                    if (j < c-1 && grid[i][j+1] == 1) perimeter -= 2;//left
+                }
+            }
+        }
+        return perimeter; 
+    }
+}
