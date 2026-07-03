cf19cd93d277a7fc7c2294aa7c092dfc47791325
KIM DOYOON <115680101+doyoon323@users.noreply.github.com>
2026-07-03 23:32:48 +0900

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        
+        if(n==1) return nums[0];
+
+        dp[0]=0;
+        dp[1]=nums[1];
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        int max = dp[n-1];
+
+        dp[0]=nums[0];
+        dp[1]=nums[0];
+        for(int i=2;i<n-1;i++){
+            dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+
+        return max > dp[n-2] ? max : dp[n-2];
+    }
+}
