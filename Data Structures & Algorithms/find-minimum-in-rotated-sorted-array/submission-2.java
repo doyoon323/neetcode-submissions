e1654a56e20c4aa01605190a3f8140f99f7543ef
KIM DOYOON <115680101+doyoon323@users.noreply.github.com>
2026-07-09 19:03:11 +0900

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int res = nums[0]; 
+        int left=0, right = nums.length-1; 
+
+
+// mid가 min보다 작다? -> rot되어있으며, 0초과 mid미만을 훑어야함.
+// mid가 min보다 크다? -> 0~mid까지 오름차순 정렬되어있으며, mid초과 right까지 살펴보아야한다. 
+        while(left<=right){
+            //[4,5,6,7,0,1,2]
+            res = Math.min(res,nums[left]);
+            int mid=(left+right)/2; 
+            res = Math.min(res,nums[mid]);
+            System.out.println("left,right, res: " + left+" " +right+" "+ res);
+
+            if(nums[mid]< nums[left]) right = mid-1;
+            else left = mid+1; 
+        }
+
+        return res; 
+    }
+}
