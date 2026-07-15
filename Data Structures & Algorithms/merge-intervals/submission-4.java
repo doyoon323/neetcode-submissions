9236a8653f0a64ba650483401f211fff6c42d12c
KIM DOYOON <115680101+doyoon323@users.noreply.github.com>
2026-07-15 19:20:46 +0900

    Add: merge-intervals - submission-4

---
+++ Data Structures & Algorithms/merge-intervals/submission-4.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length==1) return intervals; 
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
+        int n = 0;
+        for(int i=1;i<intervals.length;i++){
+            int[] cur = intervals[i];
+
+            if(intervals[n][1]>=cur[0]) {
+                int max = Math.max(intervals[n][1],cur[1]);
+                intervals[n][1]=max;
+            }
+            else {
+                n++;
+                intervals[n] = intervals[i];
+            }
+        }
+        int[][] res = new int[n+1][2];
+        for(int i=0;i<=n;i++){
+            res[i][0]=intervals[i][0];
+            res[i][1]=intervals[i][1];
+        }
+
+        return res; 
+    }
+}
