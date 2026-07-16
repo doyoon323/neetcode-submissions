2daaa659ef73da43fe17ad32bb0fe08cfa4e77fa
KIM DOYOON <115680101+doyoon323@users.noreply.github.com>
2026-07-16 17:32:57 +0900

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>(); 
+        for(int i=0;i<numCourses;i++) adj.add(new ArrayList<>());
+
+        for(int[] c : prerequisites) {
+            indegree[c[0]]++;
+            adj.get(c[0]).add(c[1]);
+            adj.get(c[1]).add(c[0]);
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i=0;i<numCourses;i++) 
+            if(indegree[i]==0) q.offer(i);
+
+        int n=0; 
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            n++;
+
+            for(int node: adj.get(cur)){
+                if(--indegree[node]==0) q.offer(node);
+            }
+        }
+        return n==numCourses; 
+    }
+}
