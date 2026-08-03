6776e408a50877e01a02f45ccaa83698f5648467
KIM DOYOON <115680101+doyoon323@users.noreply.github.com>
2026-08-04 02:42:45 +0900

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    List<List<String>> res = new ArrayList<>();
+
+    public List<List<String>> partition(String s) {
+        backtrack(s, 0, new ArrayList<>());
+        return res;
+    }
+
+    private void backtrack(String s, int start, List<String> path) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            if (isPalindrome(s, start, i)) {
+                path.add(s.substring(start, i + 1));
+                backtrack(s, i + 1, path);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
