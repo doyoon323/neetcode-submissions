d9e1c9b5a220ec00853ff62ab2a981bc968020ab
KIM DOYOON <115680101+doyoon323@users.noreply.github.com>
2026-08-10 00:07:46 +0900

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        double[][] cars = new double[position.length][2];
+
+        for (int i = 0; i < position.length; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = (double)(target-position[i])/speed[i];
+        } 
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+        
+        int fleet=1; 
+        double prev = cars[0][1]; 
+        
+        for (int i =1; i < position.length; i++) {
+            if(cars[i][1]>prev) {
+                fleet++; 
+                prev = cars[i][1];
+            }
+        }
+        return fleet; 
+    }
+
+}
