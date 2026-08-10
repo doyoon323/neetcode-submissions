f465d6d32762dbca49be88557eb33ec86f0d425c
KIM DOYOON <115680101+doyoon323@users.noreply.github.com>
2026-08-10 15:02:46 +0900

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int total=0;
+        for(int x:nums) total+=x; 
+        if(total%2!=0) return false; 
+
+        boolean[] dp = new boolean[total/2+1];
+        dp[0]=true;
+
+        for(int x : nums){
+            for(int i=total/2;i>=0;i--){
+                if(i-x>=0){
+                    dp[i] = dp[i] || dp[i-x];
+                }
+            }
+            if(dp[total/2])return true;
+        }
+        return dp[total/2];
+    }
+}
